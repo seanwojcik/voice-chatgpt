15dde55e3b0ef945219907ea4a8a7d779b66af2f
Sean Wojcik <seanwojcik@users.noreply.github.com>
2023-06-20 20:26:34 -0400

    Update chatgpt_api.py

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -4,13 +4,9 @@ import os
 import openai
 
 
-def setup_openai_api(api_key):
-    openai.api_key = api_key
-
-
 def chat_gpt(messages, api_key=None, description=None):
     if api_key is not None:
-        setup_openai_api(api_key)
+        openai.api_key = api_key
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
